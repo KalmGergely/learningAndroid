1a71500275bdd2bbc1fc2211110be9c77a5a6d67
Gergely Kalman <kalm.gergely@gmail.com>
2023-02-16 16:08:53 +0100

    add audio file and basic functionality

--- ClapApp/app/src/main/java/com/example/clapapp/MainActivity.kt
+++ ClapApp/app/src/main/java/com/example/clapapp/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.example.clapapp
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mediaPlayer: MediaPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.clap)
+
+        val clapBtn = findViewById<Button>(R.id.btnClap)
+
+        clapBtn.setOnClickListener {
+            mediaPlayer.start()
+        }
     }
 }
