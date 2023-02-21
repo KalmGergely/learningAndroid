25eccec8b4349d0ecab18330530e01e0e19747bd
Gergely Kalman <kalm.gergely@gmail.com>
2023-02-21 15:30:29 +0100

    create basic functionality

--- ViewModelDemo/app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
+++ ViewModelDemo/app/src/main/java/com/example/viewmodeldemo/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.viewmodeldemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvCount = findViewById<TextView>(R.id.tvCount)
+        val btnCount = findViewById<Button>(R.id.btnCount)
+
+        var count = 0
+        tvCount.text = count.toString()
+
+        btnCount.setOnClickListener {
+            count++
+            tvCount.text = count.toString()
+        }
+
     }
 }
