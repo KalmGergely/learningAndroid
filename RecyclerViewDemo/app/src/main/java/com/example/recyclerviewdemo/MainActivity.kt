5c56922a161c77c781ba0148631bad3c113b56f5
Gergely Kalman <kalm.gergely@gmail.com>
2023-02-24 12:45:20 +0100

    create simple list for the recyclerview

--- RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.kt
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MainActivity.kt
@@ -7,11 +7,14 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    val fruitList = listOf<String>("Mango", "Apple", "Banana", "Lemon")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.myRecyclerView)
         recyclerView.setBackgroundColor(Color.YELLOW)
         recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = MyRecyclerViewAdapter(fruitList)
     }
 }
--- RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MyRecyclerViewAdapter.kt
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MyRecyclerViewAdapter.kt
@@ -1,21 +1,29 @@
 package com.example.recyclerviewdemo
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class MyRecyclerViewAdapter : RecyclerView.Adapter<MyViewHolder>() {
+class MyRecyclerViewAdapter(val fruitList: List<String>) : RecyclerView.Adapter<MyViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        TODO("Not yet implemented")
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val listItem = layoutInflater.inflate(R.layout.list_item, parent, false)
+
+        return MyViewHolder(listItem)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val fruit = fruitList[position]
+        holder.myTextView.text = fruit
     }
 
     override fun getItemCount(): Int {
-        return 5
+        return fruitList.size
     }
 }
 
-class MyViewHolder(val view : View) : RecyclerView.ViewHolder(view){}
+class MyViewHolder(val view : View) : RecyclerView.ViewHolder(view) {
+    val myTextView = view.findViewById<TextView>(R.id.tvName)
+}
