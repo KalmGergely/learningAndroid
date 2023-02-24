26bae8480807a7a4bf32e8ec9369d4336919ec89
Gergely Kalman <kalm.gergely@gmail.com>
2023-02-24 12:30:19 +0100

    list item layout

---
+++ RecyclerViewDemo/app/src/main/java/com/example/recyclerviewdemo/MyRecyclerViewAdapter.kt
@@ -0,0 +1,21 @@
+package com.example.recyclerviewdemo
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class MyRecyclerViewAdapter : RecyclerView.Adapter<MyViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return 5
+    }
+}
+
+class MyViewHolder(val view : View) : RecyclerView.ViewHolder(view){}
