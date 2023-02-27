7cd43fe33f9b60a6c80851e605fbb37b5d1fc1dc
Gergely Kalman <kalm.gergely@gmail.com>
2023-02-27 11:38:14 +0100

    add thread change to downloadUserData to display the download progress

--- CoroutinesDemo/app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
+++ CoroutinesDemo/app/src/main/java/com/example/coroutinesdemo1/MainActivity.kt
@@ -5,13 +5,11 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     private var count = 0
+    private lateinit var tvMessage : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +17,8 @@ class MainActivity : AppCompatActivity() {
         val textView = findViewById<TextView>(R.id.tvCount)
         val countButton = findViewById<Button>(R.id.btnCount)
         val downloadButton = findViewById<Button>(R.id.btnDownload)
+
+        tvMessage = findViewById(R.id.tvMessage)
 
         countButton.setOnClickListener {
             textView.text = count++.toString()
@@ -29,11 +29,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-}
 
-private suspend fun downloadUserData() {
-    for (i in 1..200000) {
-        Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
-        delay(100)
+    private suspend fun downloadUserData() {
+        for (i in 1..200000) {
+            Log.i("MyTag", "Downloading user $i in ${Thread.currentThread().name}")
+            withContext(Dispatchers.Main) {
+                tvMessage.text = "Downloading user $i"
+            }
+            delay(100)
+        }
     }
 }
+
---
+++ CoroutinesDemo/app/src/main/java/com/example/coroutinesdemo1/SuspendDemo.kt
@@ -0,0 +1,8 @@
+package com.example.coroutinesdemo1
+
+class SuspendDemo {
+
+    private fun firstFunction() {}
+
+    private suspend fun secondFunction() {}
+}
