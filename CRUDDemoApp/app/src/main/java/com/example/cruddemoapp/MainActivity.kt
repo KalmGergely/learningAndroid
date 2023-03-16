f98cc6154f360793a5548e3e1e64d9aefe0c786f
Gergely Kalman <kalm.gergely@gmail.com>
2023-03-16 13:14:26 +0100

    add functionality to display the users on the app

--- CRUDDemoApp/app/src/main/java/com/example/cruddemoapp/MainActivity.kt
+++ CRUDDemoApp/app/src/main/java/com/example/cruddemoapp/MainActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.cruddemoapp.db.AppDB
 import com.example.cruddemoapp.db.User
 
@@ -13,7 +15,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var etEmail: EditText
     private lateinit var btnSave: Button
     private lateinit var btnClear: Button
+
     private lateinit var viewModel: UserViewModel
+    private lateinit var userRecyclerView: RecyclerView
+    private lateinit var adapter: UserRecyclerViewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,6 +28,8 @@ class MainActivity : AppCompatActivity() {
         etEmail = findViewById(R.id.etEmail)
         btnSave = findViewById(R.id.btnSave)
         btnClear = findViewById(R.id.btnClear)
+
+        userRecyclerView = findViewById(R.id.rvUser)
 
         val dao = AppDB.getInstance(application).userDao()
         val factory = UserViewModelFactory(dao)
@@ -35,6 +43,8 @@ class MainActivity : AppCompatActivity() {
         btnClear.setOnClickListener() {
             clearUserData()
         }
+
+        initRecyclerView()
     }
 
     private fun saveUserData() {
@@ -47,5 +57,19 @@ class MainActivity : AppCompatActivity() {
     private fun clearUserData() {
         etName.text.clear()
         etEmail.text.clear()
+    }
+
+    private fun initRecyclerView() {
+        userRecyclerView.layoutManager = LinearLayoutManager(this)
+        adapter = UserRecyclerViewAdapter()
+        userRecyclerView.adapter = adapter
+        displayUsers()
+    }
+
+    private fun displayUsers() {
+        viewModel.users.observe(this) {
+            adapter.setList(it)
+            adapter.notifyDataSetChanged()
+        }
     }
 }
