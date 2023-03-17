ebcec942fdb3b3ca3ccf199ff27a0b61b4a49cb1
Gergely Kalman <kalm.gergely@gmail.com>
2023-03-17 11:08:25 +0100

    apply viewbinding to mainactivity

--- ViewBindingDemo/app/src/main/java/com/example/viewbindingdemo/MainActivity.kt
+++ ViewBindingDemo/app/src/main/java/com/example/viewbindingdemo/MainActivity.kt
@@ -5,20 +5,27 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.example.viewbindingdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        val tvMessage = findViewById<TextView>(R.id.tvMessage)
-        val etName = findViewById<EditText>(R.id.etName)
-        val btnSubmit = findViewById<Button>(R.id.btnSubmit)
+        //val tvMessage = findViewById<TextView>(R.id.tvMessage)
+        //val etName = findViewById<EditText>(R.id.etName)
+        //val btnSubmit = findViewById<Button>(R.id.btnSubmit)
 
-        btnSubmit.setOnClickListener() {
-            val name = etName.text.toString()
-            tvMessage.text = "Hello $name!"
-            etName.text.clear()
+        binding.apply {
+            btnSubmit.setOnClickListener() {
+                val name = etName.text.toString()
+                tvMessage.text = "Hello $name!"
+                etName.text.clear()
+            }
         }
     }
 }
