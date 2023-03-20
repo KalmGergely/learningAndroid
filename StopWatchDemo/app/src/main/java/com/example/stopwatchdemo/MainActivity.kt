ec5b8041c77a5ff5890814cb20a7387760e15a0d
Gergely Kalman <kalm.gergely@gmail.com>
2023-03-20 09:02:28 +0100

    create basic timer functionality

--- StopWatchDemo/app/src/main/java/com/example/stopwatchdemo/MainActivity.kt
+++ StopWatchDemo/app/src/main/java/com/example/stopwatchdemo/MainActivity.kt
@@ -1,11 +1,67 @@
 package com.example.stopwatchdemo
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.stopwatchdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var isStarted = false
+    private lateinit var serviceIntent: Intent
+    private var time = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        serviceIntent = Intent(applicationContext,StopWatchService::class.java)
+        registerReceiver(updateTime, IntentFilter(StopWatchService.CURRENT_TIME))
+
+        binding.apply {
+            btnStartStop.setOnClickListener() {
+                startOrStop()
+            }
+
+            btnReset.setOnClickListener() {
+                reset()
+            }
+        }
+    }
+
+    private fun startOrStop() {
+        if (isStarted)
+            stop()
+        else
+            start()
+    }
+
+    private fun start() {
+        serviceIntent.putExtra(StopWatchService.CURRENT_TIME, time)
+        startService(serviceIntent)
+        binding.btnStartStop.text = "Stop"
+        isStarted = true
+    }
+
+    private fun stop() {
+        stopService(serviceIntent)
+        binding.btnStartStop.text = "Start"
+        isStarted = false
+    }
+
+    private fun reset() {
+
+    }
+
+    private val updateTime: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            time = intent.getDoubleExtra(StopWatchService.CURRENT_TIME, 0.0)
+            binding.tvTime.text = time.toString()
+        }
+
     }
 }
