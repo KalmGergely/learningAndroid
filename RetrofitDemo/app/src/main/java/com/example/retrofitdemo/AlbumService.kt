44bcd2875e957553f344ab1de97e8a18b70344dc
Gergely Kalman <kalm.gergely@gmail.com>
2023-03-21 11:43:35 +0100

    add separate service functions for query param and pathvariable handling, refactor mainactivity.kt

--- RetrofitDemo/app/src/main/java/com/example/retrofitdemo/AlbumService.kt
+++ RetrofitDemo/app/src/main/java/com/example/retrofitdemo/AlbumService.kt
@@ -2,8 +2,16 @@ package com.example.retrofitdemo
 
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface AlbumService {
     @GET("/albums")
     suspend fun getAlbums(): Response<Albums>
+
+    @GET("/albums")
+    suspend fun getAlbumsByUser(@Query("userId") userId: Int): Response<Albums>
+
+    @GET("/albums/{id}")
+    suspend fun getAlbumsById(@Path("id") albumId: Int): Response<AlbumItem>
 }
--- RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
+++ RetrofitDemo/app/src/main/java/com/example/retrofitdemo/MainActivity.kt
@@ -10,18 +10,25 @@ import androidx.lifecycle.liveData
 import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var retService: AlbumService
+    private lateinit var tvItems: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvItems = findViewById<TextView>(R.id.tvItems)
+        tvItems = findViewById<TextView>(R.id.tvItems)
 
-        val retService = RetrofitInstance
+        retService = RetrofitInstance
             .getRetrofitInstance()
             .create(AlbumService::class.java)
 
+        getAblbumsByUserId(3)
+        logAlbumById(1)
+    }
+
+    private fun getAblbumsByUserId(id: Int) {
         val responseLiveData: LiveData<Response<Albums>> = liveData {
-            emit(retService.getAlbums())
+            emit(retService.getAlbumsByUser(id))
         }
 
         responseLiveData.observe(this, Observer {
@@ -30,11 +37,25 @@ class MainActivity : AppCompatActivity() {
             if(albumList != null)
                 while (albumList.hasNext()) {
                     val albumItem = albumList.next()
-                    val result = " " + "Album Title : ${albumItem.title}" + "\n" +
-                            " " + "Album id : ${albumItem.id}" + "\n" +
-                            " " + "User id : ${albumItem.userId}" + "\n\n\n"
+                    val result = "Album title: ${albumItem.title}\nAlbum id: ${albumItem.id}\nAlbum userId: ${albumItem.userId}\n\n"
                     tvItems.append(result)
                 }
+        })
+    }
+
+    private fun logAlbumById(id: Int) {
+        val pathResponse: LiveData<Response<AlbumItem>> = liveData {
+            emit(retService.getAlbumsById(id))
+        }
+
+        pathResponse.observe(this, Observer {
+            val album: AlbumItem? = it.body()
+
+            if(album != null) {
+                val result =
+                    "Album title: ${album.title}\nAlbum id: ${album.id}\nAlbum userId: ${album.userId}\n\n"
+                Log.i("Pathvariable practice", result)
+            }
         })
     }
 }
