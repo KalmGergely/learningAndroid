e97051cedd3ae6f7ab2da24c0ccc95cb11888112
Gergely Kalman <kalm.gergely@gmail.com>
2023-03-27 15:33:54 +0200

    add action between the  fragments, create animation xml files

--- NavDemo/app/src/main/java/com/example/navdemo/HomeFragment.kt
+++ NavDemo/app/src/main/java/com/example/navdemo/HomeFragment.kt
@@ -1,10 +1,14 @@
 package com.example.navdemo
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
 import com.example.navdemo.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -20,6 +24,16 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.btnSubmit.setOnClickListener() {
+            if (!TextUtils.isEmpty(binding.etName.text.toString())) {
+                val bundle = bundleOf("user_input" to binding.etName.text.toString())
+                it.findNavController().navigate(R.id.action_homeFragment_to_secondFragment, bundle)
+            } else {
+                Toast.makeText(activity,"Please provide a name before submission", Toast.LENGTH_LONG).show()
+            }
+        }
+
         return binding.root
     }
 }
